d853c093d2d36a5c43f8349a8905d67e4f87a6fd
bexkysx <xu.song@centre.edu>
2020-04-19 16:44:10 -0400

    add canvas & css code & js code 1st

---
+++ jscode.js
@@ -0,0 +1,25 @@
+var test = window.devicePixelRatio
+            console.log(test);
+
+window.onload = function () {
+    var oC = document.getElementById('c1');
+    var oCG = oC.getContext('2d');
+    
+    oC.onmousedown = function (ev) {
+        var ev = ev || window.event;
+        oCG.moveTo(ev.clientX - oC.offsetLeft, ev.clientY - oC.offsetTop);
+        //ev.clientX-oC.offsetLeft,ev.clientY-oC.offsetTop鼠标在当前画布上X,Y坐标
+        
+        document.onmousemove = function (ev) {
+                var ev = ev || window.event;//获取event对象
+                oCG.lineTo(ev.clientX - oC.offsetLeft, ev.clientY - oC.offsetTop);
+                oCG.stroke();
+                    };
+        
+        oC.onmouseup = function () {
+                document.onmousemove = null;
+                document.onmouseup = null;
+                };
+        
+            };
+    };
